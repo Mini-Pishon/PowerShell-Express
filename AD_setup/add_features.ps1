c5db7df973ac7dfa46ab419320046119d99e78ce
Mini-Pishon <miistygrey@gmail.com>
2025-04-01 18:31:23 +0200

    update script

--- AD_setup/add_features.ps1
+++ AD_setup/add_features.ps1
@@ -19,9 +19,4 @@ foreach ($Feature in $FeatureList) {
       Write-Output "$Feature is already installed or not available for installation."
    }
 }
-# Schedule task to install new features after restart
-$ScriptPath = "c:\scripts\Forest_config.ps1"
-$TaskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"$ScriptPath`""
-$TaskTrigger = New-ScheduledTaskTrigger -AtStartup
-$TaskPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
-Register-ScheduledTask -Action $TaskAction -Trigger $TaskTrigger -Principal $TaskPrincipal -TaskName "InstallForest"
+
